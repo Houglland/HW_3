ba7db68bccc122dfb5acd6ae7b6681ff3005f8f0
Kiryl Bayarenka <kboyarenko25@gmail.com>
2024-11-05 21:16:35 +0100

    Добавил автоформатирование

--- test_google_search.py
+++ test_google_search.py
@@ -3,17 +3,17 @@ from selene import browser, be, have, by
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 
+
 @pytest.fixture(scope='function', autouse=True)
 def browser_setup():
-    options = webdriver.ChromeOptions()
-    options.add_argument('--window-size=1200,800')
-    driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
-    browser.set_driver(driver)
+    browser.config.window_width = 1920
+    browser.config.window_height = 1080
     yield
     browser.quit()
 
+
 def test_google_search():
-    browser.open_url('https://google.com')
+    browser.open_url('https://google.com/ncr')
 
     if browser.element(by.text('Принять все')).matching(be.visible):
         browser.element(by.text('Принять все')).click()
@@ -24,8 +24,9 @@ def test_google_search():
     search_results = browser.element('[id="search"]')
     search_results.should(have.text('User-oriented Web UI browser tests in Python'))
 
+
 def test_google_search_no_results():
-    browser.open_url('https://google.com')
+    browser.open_url('https://google.com/ncr')
 
     if browser.element(by.text('Принять все')).matching(be.visible):
         browser.element(by.text('Принять все')).click()
@@ -35,5 +36,3 @@ def test_google_search_no_results():
 
     search_results = browser.element('[id="search"]')
     search_results.should(have.text('did not match'))
-
-
